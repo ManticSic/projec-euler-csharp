6798eefd1c328ca41c8d6587929ae14256a9fa59
Darius Weber <darius.weber@posteo.de>
2022-04-15 12:20:19 +0200

    Project Euler: Solve Problem 1

---
+++ Project Euler/Util/Numbers.cs
@@ -0,0 +1,9 @@
+﻿namespace Project_Euler.Util;
+
+public static class Numbers
+{
+    public static bool IsMultipleOf(int value, int multiple)
+    {
+        return value % multiple == 0;
+    }
+}
