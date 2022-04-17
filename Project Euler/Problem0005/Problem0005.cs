958eaf9f1229f7e777ee4d4723df34c2c1fb56a2
Darius Weber <darius.weber@posteo.de>
2022-04-17 17:15:56 +0200

    Solve problem #5

---
+++ Project Euler/Problem0005/Problem0005.cs
@@ -0,0 +1,30 @@
+﻿using UnityContainerAttributeRegistration.Attribute;
+
+namespace Project_Euler.Problem0005;
+
+[RegisterType]
+public class Problem0005
+{
+    public int Solve(int upperBorder)
+    {
+        List<int> dividers = Enumerable.Range(1, upperBorder)
+                                       .ToList();
+
+        int numberToTest = upperBorder;
+
+        while (true)
+        {
+            if (IsEvenlyDividable(numberToTest, dividers))
+            {
+                return numberToTest;
+            }
+
+            numberToTest += upperBorder;
+        }
+    }
+
+    private bool IsEvenlyDividable(int value, IEnumerable<int> dividers)
+    {
+        return dividers.All(divider => value % divider == 0);
+    }
+}
---
+++ ProjectEuler.Test/Problem0005Test.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using Project_Euler.Problem0005;
+
+namespace ProjectEuler.Test;
+
+/*
+ * 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+ *
+ * What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+ */
+
+public class Problem0005Test : AbstractProblemTest<Problem0005>
+{
+    [Test]
+    [TestCase(10, 2520)]
+    [TestCase(20, 232792560)]
+    public void Should(int upperBorder, int expected)
+    {
+        // prepare
+
+        // run
+        int result = Sut.Solve(upperBorder);
+
+        // verify
+        Assert.AreEqual(expected, result);
+    }
+
+    [Test]
+    [TestCase(20)]
+    public void Solve(int upperBorder)
+    {
+        // prepare
+
+        // run
+        Solve(() => Sut.Solve(upperBorder));
+    }
+}
